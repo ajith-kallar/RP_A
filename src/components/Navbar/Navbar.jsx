ac93ef7ac74257c40f1753bc45762f947cce00a0
ajith-huho <ajithkskallar@outlook.com>
2024-09-05 17:18:08 +0530

    first commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img className='logo' src={assets.logo} alt="" />
-      <img className='profile' src={assets.profile_image} alt="" />
+      <h2>ADMIN PAGE </h2>
+      {/* <img className='profile' src={assets.profile_image} alt="" /> */}
     </div>
   )
 }
